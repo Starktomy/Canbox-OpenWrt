d83b0bc276e80468fdcdbe77485c309e408e29d2
Starktomy <37720599+Starktomy@users.noreply.github.com>
2022-11-06 23:42:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,9 +10,9 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # Modify default IP
-sed -i 's/192.168.1.1/172.30.1.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/172.30.1.1/g' package/base-files/files/bin/config_generate
 # 修改机器名称
-sed -i 's/OpenWrt/CM520-79F-Two/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/CM520/g' package/base-files/files/bin/config_generate
 # 修改机器初始密码
 sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
 sed -i 's/root:x:0:0:root:/root::0:0:root:/g' package/base-files/files/etc/passwd
